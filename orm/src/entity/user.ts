01f07d05e448971571c2b4f69a8c490381a99af8
Tru Nguyen <nguyentruhn@gmail.com>
2023-09-11 09:34:57 +0700

    Db

--- orm/src/entity/user.ts
+++ orm/src/entity/user.ts
@@ -1,19 +1,25 @@
 import {Column, Entity, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
-import {Role} from "./role";
+
 
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
     id: number;
+
     @Column({type: 'varchar'})
     username: string;
+
     @Column({type: 'varchar'})
     password: string;
-    @Column({type: 'varchar'})
-    telephone: string;
-    @Column({type: 'varchar'})
-    address: string;
-    @ManyToOne(() => Role, (role) => role.id)
-    role: Role;
 
+    @Column({type: 'varchar'})
+    email: string;
+
+    @Column({type: 'varchar'})
+    role: string;
+
+    
+    @Column({type: 'varchar'})
+    avatar: string;
+   
 }
